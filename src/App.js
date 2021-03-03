48cdb795d35ffd215d7d86f0aa545fe86759daa5
michaelFeMan <mneman1702@gmail.com>
2021-03-02 20:38:12 -0600

    added state to search component. Callback from app to search so searchTerm can be passed as props to list and return filtered list

--- src/App.js
+++ src/App.js
@@ -24,23 +24,23 @@ const App = () => {
   ];
 
   const [searchTerm, setSearchTerm] =
-  React.useState('');
+    React.useState('');
 
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
+  const handleSearch = e => {
+    setSearchTerm(e.target.value);
   };
+
+  const searchedStories =
+    stories.filter(story => {
+      return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
   return (
     <div className="App">
       <h1>Hello React</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
-
+      <Search onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
 
     </div>
 
@@ -48,15 +48,24 @@ const App = () => {
   );
 };
 
-const List = props => 
+const List = props =>
   props.list.map(item => (
-      <div key="item.objectID">
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
+    <div key="item.objectID">
+      <span><a href={item.url}>{item.title}</a></span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
   ));
 
 
 export default App;
+
+const Search = props => (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.onSearch} />
+  
+    </div>
+  
+);
